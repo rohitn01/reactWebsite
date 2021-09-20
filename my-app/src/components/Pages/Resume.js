1b59c92ab007b530c0454ba834b995e77bfde16b
rohitn01 <rohitmen01@gmail.com>
2021-09-20 11:16:32 -0500

    Update Resume.js
    removed resume

--- my-app/src/components/Pages/Resume.js
+++ my-app/src/components/Pages/Resume.js
@@ -31,9 +31,6 @@ const aboutme = () => {
             <h2><img src={prom} className="prom-pic"/></h2>
             <br></br>
             <h2 className="subheader">Rohit Narayanan</h2>
-            <p className="text1">Scroll down and hover to view my resume or 
-            click <a className="resumeLink" href="https://drive.google.com/file/d/1PiUGXksFNeVbCqNuMBQdK_BR9vPqnj3G/view?usp=sharing" download>HERE</a> for a PDF copy.</p>
-            <h2><img className="mobileResume" src={resume} align="center" style={{width: '450px'}}></img></h2>
             <body className="aboutMeBody">
                
               <ul> <h3>Education:</h3>
